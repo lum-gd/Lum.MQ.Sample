e771ebfc9a45aae4e66bf050b3a9dfc024aae174
Tom Yang <lumin.yang@hotmail.com>
2022-01-31 22:40:54 +0800

    temp

---
+++ Lumin.MQ.Rabbit.WorkerSample/Program.cs
@@ -0,0 +1,10 @@
+using Lumin.MQ.Rabbit.WorkerSample;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
